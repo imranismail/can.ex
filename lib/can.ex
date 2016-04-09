0e4c554074889d3311091c68b8e84e28d5942d24
Imran Ismail <imran.codely@gmail.com>
2016-04-09 16:59:08 +0800

    Remove before_send callbacks and use macro

--- lib/can.ex
+++ lib/can.ex
@@ -1,11 +1,25 @@
 defmodule Can do
-  def controller do
+  defmacro __using__(handler) do
     quote do
       import Can.Authorizer
+      import Can
+      @unauthorized_handler unquote(handler)
     end
   end
 
-  defmacro __using__(which) when is_atom(which) do
-    apply(__MODULE__, which, [])
+  defmacro can(conn, resource \\ nil, do: block) do
+    quote do
+      case authorize(unquote(conn), unquote(resource)) do
+        {:ok, policy} ->
+          unquote(block)
+        {:error, policy} ->
+          case @unauthorized_handler do
+            {module_name, function_name} ->
+              apply(module_name, function_name, [unquote(conn), unquote(resource), policy])
+            function_name ->
+              apply(__MODULE__, function_name, [unquote(conn), unquote(resource), policy])
+          end
+      end
+    end
   end
 end
--- lib/can/authorizer.ex
+++ lib/can/authorizer.ex
@@ -1,32 +1,23 @@
 defmodule Can.Authorizer do
   import Can.Helper
-  import Plug.Conn, only: [put_private: 3]
-  require IEx
 
+  @doc """
+  Authorize headers
+  """
   def authorize(conn, args) when is_list(args), do: authorize(conn, nil, args)
   def authorize(conn, resource) when is_map(resource), do: authorize(conn, nil, resource)
-
   def authorize(conn, action \\ nil, args \\ [])
+
   @doc """
   Used with a changeset
   """
   def authorize(conn, action, %Ecto.Changeset{} = changeset) do
     action = action || fetch_private!(conn, :phoenix_action)
-    policy =
-      changeset.model.__struct__
-      |> policy_module
-
-    trusted = apply_policy(policy, action, [conn, changeset])
-
-    if trusted do
-      conn
-      |> put_private(:can_authorized, true)
-    else
-      conn
-      |> put_private(:can_authorized, false)
-      |> put_private(:can_policy, policy)
-    end
+    policy = policy_module(changeset.model.__struct__)
+    authorized = apply_policy(policy, action, [conn, changeset])
+    if authorized, do: {:ok, policy}, else: {:error, policy}
   end
+
   @doc """
   Used without a resource
   """
@@ -35,36 +26,17 @@ defmodule Can.Authorizer do
     policy = conn
       |> fetch_private!(:phoenix_controller)
       |> policy_module("Controller")
-
-    trusted = apply_policy(policy, action, [conn] ++ args)
-
-    if trusted do
-      conn
-      |> put_private(:can_authorized, true)
-    else
-      conn
-      |> put_private(:can_authorized, false)
-      |> put_private(:can_policy, policy)
-    end
+    authorized = apply_policy(policy, action, [conn] ++ args)
+    if authorized, do: {:ok, policy}, else: {:error, policy}
   end
+
   @doc """
   Used with a model
   """
   def authorize(conn, action, model) when is_map(model) do
     action = action || fetch_private!(conn, :phoenix_action)
-    policy =
-      model.__struct__
-      |> policy_module
-
-    trusted = apply_policy(policy, action, [conn, model])
-
-    if trusted do
-      conn
-      |> put_private(:can_authorized, true)
-    else
-      conn
-      |> put_private(:can_authorized, false)
-      |> put_private(:can_policy, policy)
-    end
+    policy = policy_module(model.__struct__)
+    authorized = apply_policy(policy, action, [conn, model])
+    if authorized, do: {:ok, policy}, else: {:error, policy}
   end
 end
