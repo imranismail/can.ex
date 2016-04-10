327bb046e1a4d8305450155413b3b98e57bfca38
Imran Ismail <imran.codely@gmail.com>
2016-04-10 12:20:10 +0800

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Can.Mixfile do
 
   def project do
     [app: :can,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
