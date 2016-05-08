1be5728df1875d60b34fa3bbef935877f82205b2
Imran Ismail <imran.codely@gmail.com>
2016-05-09 01:13:02 +0800

    Update can.ex to rework on the can macro
    - Override action plug
    - Reworked can macro and removed block
    
    TODO: Rework authorizer.ex to accept new function head, a connection, action and resource. A resource must strictly be a struct

--- lib/can.ex
+++ lib/can.ex
@@ -1,24 +1,36 @@
 defmodule Can do
-  defmacro __using__(handler) do
+  defmacro __using__(opts) do
     quote do
+      @unauthorized_handler opts[:handler] || raise Can.NoHandlerError
+      @unauthorized_module  opts[:module]  || __MODULE__
+
       import Can.Authorizer
       import Can
-      @unauthorized_handler unquote(handler)
+
+      def action(conn, _), do 
+        try do
+          args = [conn, conn.params]
+          apply(__MODULE__, action_name(conn), args)
+        rescue
+          error in Can.UnauthorizedError ->
+            args = [conn, error[:resource], error[:policy]]
+            apply(@unauthorized_module, @unauthorized_function, args)
+        end
+      end
+
+      defoverridable action: 2
     end
   end
 
-  defmacro can(conn, resource \\ nil, do: block) do
+  defmacro can(conn, [{action, resource}] \\ nil) do
     quote do
-      case authorize(unquote(conn), unquote(resource)) do
-        {:ok, policy} ->
-          unquote(block)
-        {:error, policy} ->
-          case @unauthorized_handler do
-            {module_name, function_name} ->
-              apply(module_name, function_name, [unquote(conn), unquote(resource), policy])
-            function_name ->
-              apply(__MODULE__, function_name, [unquote(conn), unquote(resource), policy])
-          end
+      case authorize(unquote(conn), unquote(action), unquote(resource)) do
+        {:ok, _}         -> 
+          conn
+        {:error, policy} -> 
+          raise Can.UnauthorizedError,
+            resource: unquote(resource), 
+            policy: policy
       end
     end
   end
