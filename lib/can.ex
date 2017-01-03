e8beca38a2e2a5f7008d4850d038b8906ff4298e
Imran Ismail <imran.codely@gmail.com>
2017-01-03 16:31:41 +0800

    Update can

--- lib/can.ex
+++ lib/can.ex
@@ -1,25 +1,112 @@
 defmodule Can do
-  defmacro __using__(handler) do
-    quote do
-      import Can.Authorizer
-      import Can
-      @unauthorized_handler unquote(handler)
+  defstruct [
+    policy: nil,
+    action: nil,
+    authorized?: false
+  ]
+
+  use Plug.Builder
+
+  def call(conn, _opts) do
+    action = fetch_action!(conn)
+    policy = fetch_module!(conn)
+    can    = struct(__MODULE__, action: action, policy: policy)
+    put_private(conn, :can, can)
+  end
+
+  def can(conn, action \\ nil, context \\ []) when is_atom(action) and is_list(context) do
+    policy      = get_policy(conn)
+    action      = action || get_action(conn)
+    authorized? = apply_policy(policy, action, context)
+
+    if authorized? do
+      authorize(conn)
+    else
+      raise Can.UnauthorizedError, action: action, context: context
     end
   end
 
-  defmacro can(conn, resource \\ nil, do: block) do
-    quote do
-      case authorize(unquote(conn), unquote(resource)) do
-        {:ok, policy} ->
-          unquote(block)
-        {:error, policy} ->
-          case @unauthorized_handler do
-            {module_name, function_name} ->
-              apply(module_name, function_name, [unquote(conn), unquote(resource), policy])
-            function_name ->
-              apply(__MODULE__, function_name, [unquote(conn), unquote(resource), policy])
-          end
-      end
+  def authorize(conn, boolean \\ true) do
+    put_can(:authorized?, boolean)
+  end
+
+  def put_policy(conn, policy) do
+    put_private(conn, :can, put_can(conn, :policy, policy))
+  end
+
+  def get_policy(conn) do
+    conn.private[:can].policy
+  end
+
+  def put_action(conn, action) do
+    put_private(conn, :can, put_can(conn, :action, action))
+  end
+
+  def get_action(conn) do
+    conn.private[:can].action
+  end
+
+  defp put_can(conn, key, value) do
+    conn.private
+    |> Map.get(:can, %Can{})
+    |> Map.put(key, value)
+  end
+
+  defp apply_policy!(policy, function, args) do
+    policy
+    |> verify_policy!
+    |> apply(function, args)
+  end
+
+  defp verify_policy!(policy) do
+    if Code.ensure_loaded?(policy) do
+      policy
+    else
+      raise Can.UndefinedPolicyError, policy: policy
+    end
+  end
+
+  defp verify_phoenix_deps! do
+    unless Code.ensure_loaded?(Phoenix) do
+      raise Can.PhoenixNotLoadedError
+    end
+  end
+
+  defp fetch_action!(conn) do
+    Map.fetch!(conn.private, :phoenix_action)
+  end
+
+  defp fetch_module!(conn) do
+    conn.private
+    |> Map.fetch!(:phoenix_controller)
+    |> infer_policy("Controller")
+  end
+
+  defp infer_policy(module, suffix \\ "") do
+    module_parts = Module.split(module)
+
+    policy =
+      module_parts
+      |> List.last
+      |> unsuffix(suffix)
+      |> suffix("Policy")
+
+    module_parts
+    |> List.replace_at(length(module_parts) - 1, policy)
+    |> Module.concat
+  end
+
+  defp suffix(prefix, suffix) do
+    prefix <> suffix
+  end
+
+  defp unsuffix(value, suffix) do
+    string = to_string(value)
+    suffix_size = byte_size(suffix)
+    prefix_size = byte_size(string) - suffix_size
+    case string do
+      <<prefix::binary-size(prefix_size), ^suffix::binary>> -> prefix
+      _ -> string
     end
   end
 end
---
+++ lib/can/exceptions.ex
@@ -0,0 +1,34 @@
+defmodule Can.UndefinedPolicyError do
+  defexception [
+    plug_status: 500,
+    message: "Undefined policy",
+    policy: nil
+  ]
+
+  def exception(opts) do
+    policy = Keyword.fetch!(opts, :policy)
+    struct(__MODULE__, message: "Undefined policy: #{prettify_module(policy)}", policy: policy)
+  end
+
+  defp prettify_module(module) do
+    module
+    |> Module.split()
+    |> Enum.join(".")
+  end
+end
+
+defmodule Can.PhoenixNotLoadedError do
+  defexception [
+    plug_status: 500
+    message: "you tried to use `Can` but it requires `Phoenix` to be loaded as a dependency"
+  ]
+end
+
+defmodule Can.UnauthorizedError do
+  defexception [
+    plug_status: 401,
+    message: "This connection is not authorized to perform this action",
+    action: action,
+    context: context
+  ]
+end
